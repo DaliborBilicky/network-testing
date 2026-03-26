f485c5296fcadf861a8a36c2809effffc6fe395a
Dalibor Bilický <d.bilicky@gmail.com>
2026-03-26 19:06:06 +0100

    Data holding objects

---
+++ src/main/java/network/testing/core/model/DistanceMatrix.java
@@ -0,0 +1,19 @@
+package network.testing.core.model;
+
+public class DistanceMatrix {
+	private final int n;
+	private final double[] distances;
+
+	public DistanceMatrix(int n, double[] rawDistances) {
+		this.n = n;
+		this.distances = rawDistances.clone();
+	}
+
+	public double getDistance(int u, int v) {
+		return this.distances[u * this.n + v];
+	}
+
+	public int getSize() {
+		return this.n;
+	}
+}
